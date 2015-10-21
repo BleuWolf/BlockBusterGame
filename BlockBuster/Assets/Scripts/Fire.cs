8fa2b58ad6cffb132f8fbce028551cd7e99b2c1d
BleuWolf <sramirez1031@stu.neisd.net>
2015-10-21 11:16:14 -0500

    Added Block Breaker Game

---
+++ BlockBuster/Assets/Scripts/Fire.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour
+{
+	
+	public Rigidbody SpherePrefab;
+	public Transform barrelEnd;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Use this for initialization
+	void Update ()
+	{
+		int fingerCount = 0;
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+				fingerCount++;
+			if (fingerCount > 0){
+				Vector3 worldPoint = Camera.main.ScreenToWorldPoint(touch.position);
+				Vector2 touchPos = new Vector2(worldPoint .x, worldPoint.y);
+				transform.LookAt(touchPos);
+				
+				Rigidbody rocketInstance;
+				rocketInstance = Instantiate(SpherePrefab, touchPos, Quaternion.identity) as Rigidbody;
+				rocketInstance.AddForce(barrelEnd.forward * 7000);
+				
+			}
+		}
+	}
+}
